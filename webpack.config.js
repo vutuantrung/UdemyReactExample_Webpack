1a3d8772cb96b2d0189ad423cb1f26a7683bdfd5
VUTuanTrung <trung.lespace@gmail.com>
2021-01-08 21:10:23 +0100

    adding files and Babel, loading CSS file, modify package.json

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: ''
+    },
+    devtool: 'cheap-module-eval-source-map',
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+        }, {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use: [{
+                    loader: 'style-loader'
+                },
+                {
+                    loader: 'css-loader',
+                    options: {
+                        importLoaders: 1,
+                        modules: {
+                            localIdentName: '[name]__[local]__[hash:base64:5]'
+                        }
+                    }
+                },
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        ident: 'postcss',
+                        plugins: () => [autoprefixer()]
+                    }
+                }
+            ]
+        }]
+    }
+};
